4a8c34bd282f81eca39f2c823289f4268949fb74
George Nash <george.nash@intel.com>
2020-03-02 12:15:06 -0800

    Introspection added to smart_home_server update based on code review
    This changed the printf statments to PRINT statments
    
    Checks the return value of the fread function.
    
    Signed-off-by: George Nash <george.nash@intel.com>

--- apps/smart_home_server_linux.c
+++ apps/smart_home_server_linux.c
@@ -71,29 +71,33 @@ app_init(void)
 
   err |= oc_add_device("/oic/d", "oic.d.switch", "Temp_sensor", "ocf.2.0.5",
                        "ocf.res.1.3.0,ocf.sh.1.3.0", NULL, NULL);
-  printf("\tSwitch device added.\n");
+  PRINT("\tSwitch device added.\n");
 #if defined(OC_IDD_API)
   FILE *fp;
   uint8_t *buffer;
   size_t buffer_size;
-
+  const char introspection_error[] =
+    "\tERROR Could not read smart_home_server_linux_IDD.cbor\n"
+    "\tIntrospection data not set for device.\n";
   fp = fopen("./smart_home_server_linux_IDD.cbor", "rb");
   if (fp) {
     fseek(fp, 0, SEEK_END);
     buffer_size = ftell(fp);
     rewind(fp);
 
-    buffer = (uint8_t *)malloc(buffer_size *
-                               sizeof(uint8_t)); // Enough memory for the file
-    fread(buffer, buffer_size, 1, fp);           // Read in the entire file
-    fclose(fp);                                  // Close the file
+    buffer = (uint8_t *)malloc(buffer_size * sizeof(uint8_t));
+    size_t fread_ret = fread(buffer, buffer_size, 1, fp);
+    fclose(fp);
 
-    oc_set_introspection_data(0, buffer, buffer_size);
-    printf("\tIntrospection data set for device.\n");
+    if (fread_ret == 1) {
+      oc_set_introspection_data(0, buffer, buffer_size);
+      PRINT("\tIntrospection data set for device.\n");
+    } else {
+      PRINT("%s", introspection_error);
+    }
     free(buffer);
   } else {
-    printf("\tERROR Could not read smart_home_server_linux_IDD.cbor\n");
-    printf("\tIntrospection data not set for device.\n");
+    PRINT("%s", introspection_error);
   }
 #endif
 
@@ -539,7 +543,7 @@ register_resources(void)
   oc_resource_set_request_handler(temp_resource, OC_GET, get_temp, NULL);
   oc_resource_set_request_handler(temp_resource, OC_POST, post_temp, NULL);
   oc_add_resource(temp_resource);
-  printf("\tTemperature resource added.\n");
+  PRINT("\tTemperature resource added.\n");
   bswitch = oc_new_resource(NULL, "/switch", 1, 0);
   oc_resource_bind_resource_type(bswitch, "oic.r.switch.binary");
   oc_resource_bind_resource_interface(bswitch, OC_IF_A);
@@ -549,7 +553,7 @@ register_resources(void)
   oc_resource_set_request_handler(bswitch, OC_GET, get_switch, NULL);
   oc_resource_set_request_handler(bswitch, OC_POST, post_switch, NULL);
   oc_add_resource(bswitch);
-  printf("\tSwitch resource added.\n");
+  PRINT("\tSwitch resource added.\n");
 #ifdef OC_COLLECTIONS
   col = oc_new_collection(NULL, "/platform", 1, 0);
   oc_resource_bind_resource_type(col, "oic.wk.col");
@@ -573,7 +577,7 @@ register_resources(void)
   oc_resource_set_properties_cbs(col, get_platform_properties, NULL,
                                  set_platform_properties, NULL);
   oc_add_collection(col);
-  printf("\tResources added to collection.\n");
+  PRINT("\tResources added to collection.\n");
 #endif /* OC_COLLECTIONS */
 }
 
@@ -736,11 +740,12 @@ main(void)
     return -1;
   }
 
-  printf("Initializing Smart Home Server.\n");
+  PRINT("Initializing Smart Home Server.\n");
   init = oc_main_init(&handler);
   if (init < 0)
     return init;
-  printf("Waiting for Client...\n");
+  PRINT("Waiting for Client...\n");
+  PRINT("Hit 'Enter' at any time to toggle switch resource\n");
   while (quit != 1) {
     next_event = oc_main_poll();
     pthread_mutex_lock(&mutex);
